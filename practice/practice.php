8f235b96bf270e01c29fca5fabfca661dcfa3e9a
anu-gulhane <59767677+anu-gulhane@users.noreply.github.com>
2020-11-02 14:23:22 +0530

    Add files via upload

---
+++ practice/practice.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NOGOZO</title>
+    <link rel="stylesheet" href="css/homepage.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@600&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/f935d9d29d.js" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"></script>
+    <script scr="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@600&family=Philosopher:ital@1&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <div class="header">
+        <div class="logo">
+
+        </div>
+ 
+   
+    <div class="header2">
+        <div class="left">
+            <p>Welcome To<div class="a">SSC</p>
+        </div>
+        <div class="rightpart">
+            <img class="c rounded" src="images/undraw_exams_g4ow.png" alt="">
+        </div>
+    </div>
+        <div class="categories">
+            <h2 class="topic" style="margin-bottom:50px;margin-left:100px ;margin-top:50px;">general studies</h2>
+            <div class="conatiner">
+                <div class="row">
+                    <div class="col-sm image" style="margin-left:30px ;">
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/undraw_analyze_17kw.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <b >HISTORY</b>
+                                <a href="#" class="btn btn-primary" >Take Me There</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm image" >
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/undraw_Brainstorming_re_1lmw.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <b >ECONOMICS</b>
+                                <a href="#" class="btn btn-primary" >Take Me There</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm image" >
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/undraw_exams_g4ow.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <b >POLITICAL SCIENCE</b>
+                                <a href="#" class="btn btn-primary" >Take Me There</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3 image" >
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/undraw_Graduation_ktn0.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <b >GEOGRAPHY</b>
+                                <a href="#" class="btn btn-primary" >Take Me There</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>       
+        </div>
+      <div class="general studies">
+            <h2 class="topic" style="margin-bottom:50px;margin-left:100px ;margin-top:50px;">english</h2>
+            <div class="conatiner">
+                <div class="row">
+                    <div class="col-sm image" style="margin-left:30px ;">
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/undraw_learning_2q1h.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <b >ABCD</b>
+                                <a href="#" class="btn btn-primary" >Take Me There</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm image" >
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/undraw_mathematics_4otb.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <b >ABCD</b>
+                                <a href="#" class="btn btn-primary" >Take Me There</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm image" >
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/undraw_online_test_gba7.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <b >ABCD</b>
+                                <a href="#" class="btn btn-primary" >Take Me There</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3 image" >
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/undraw_professor_8lrt.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <b >ABCD</b>
+                                <a href="#" class="btn btn-primary" >Take Me There</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>            
+        </div>
+        <div class="general studies">
+            <h2 class="topic" style="margin-bottom:50px;margin-left:100px ;margin-top:50px;">logical reasoning</h2>
+            <div class="conatiner">
+                <div class="row">
+                    <div class="col-sm image" style="margin-left:30px ;">
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/undraw_Reading_book_re_kqpk.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <b> ABCD</b>
+                                <a href="#" class="btn btn-primary" >Take Me There</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm image" >
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/undraw_Scrum_board_re_wk7v.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <b >PUZZLE</b>
+                                <a href="#" class="btn btn-primary" >Take Me There</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm image" >
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/undraw_teacher_35j2.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <b >AGE</b>
+                                <a href="#" class="btn btn-primary" >Take Me There</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3 image" >
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/undraw_analyze_17kw.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <b >ABCD</b>
+                                <a href="#" class="btn btn-primary" >Take Me There</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+               
+        </div>
+
+        
+</body>
+</html>
